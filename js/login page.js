505d766bb5bc0f1177ce4aba43d7338ea7f2289b
A-gent64 <rwpatrick@proton.me>
2024-02-26 17:41:15 +0200

    update

--- js/login page.js
+++ js/login page.js
@@ -1,7 +1,5 @@
 document.getElementById("loginForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent default form submission
-
-    const email = document.getElementById("email").value;  
+     const email = document.getElementById("email").value;  
     const password = document.getElementById("pass").value; 
     const errorDiv = document.getElementById("error");
 
@@ -18,7 +16,15 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
     if (password.length < 8) { 
         errorDiv.innerHTML = "Password should be at least 8 characters long"; 
         return false; 
-    } 
+    }
+    if(password=== "admin@123")
+    {
+        let NextUlr=document.getElementById("a");
+        NextUlr.href="/dashboard/dashboard.html"
+    }
+    else{
+        NextUlr.href="/index.html"
+    }
 
     // Clear error message if there are no errors
     errorDiv.innerHTML = "";
